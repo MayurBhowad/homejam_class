6a0f1d23cdc48f302e7ae69a39afe60714ca4bda
MayurBhowad <mayurbhowad1995@gmail.com>
2021-12-10 14:52:02 +0530

    complet: connection with pg

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const PG_CONN = require('./config/pg.db');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 4001;
 
+// CONNECTION WITH POSTGRES
+PG_CONN.connect()
+    .then(() => console.log('Postgress db is up and Rolling...'))
+    .catch(err => console.log('DB_ERROR: ', err.message));
+
 app.get('/', (req, res) => {
     res.json({ success: true, route: '/' })
-});
+})
 
 app.listen(PORT, () => console.log(`Api is up and Rolling...`));
